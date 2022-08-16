a8be0e0040bd1c1c8cc22cba379421ac529d0ae6
Dominique <dfoleydev@gmail.com>
2022-08-15 21:13:20 -0400

    Added middleware folder to check routes, updates to routes, and header file in views

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // @desc  Auth with Google
 // @route GET /auth/google
+
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc  Google auth callback
 // @route GET /auth/google/callback
+
 router.get(
     '/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
@@ -15,5 +17,17 @@ router.get(
         res.redirect('/dashboard')
     },
 )
+
+// @desc Logout User
+//@route /auth/logout
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // @desc  Login/Landing page
 // @route GET /
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
@@ -11,7 +12,7 @@ router.get('/', (req, res) => {
 
 // @desc  Deshboard
 // @route GET /dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
     res.render('dashboard')
 })
 
