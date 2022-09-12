7e7e1076a9c85ae9fe8a18427d2e23c8a2d1f1b1
Dominique <dfoleydev@gmail.com>
2022-09-11 20:19:50 -0400

    created /stories

--- routes/stories.js
+++ routes/stories.js
@@ -23,4 +23,21 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
+//@desc Showw all public stories
+//@Route GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
 module.exports = router
