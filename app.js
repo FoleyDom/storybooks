eb3bbbfe14e498b861853bd2bffc3dff6b656a03
Dominique <dfoleydev@gmail.com>
2022-09-12 21:34:15 -0400

    Fully working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 //Used to store user session *below*
@@ -23,6 +24,18 @@ const app = express()
 // Body Parser
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// Method Override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    }),
+)
 
 // Logging
 // Very cool tool, use in every project
--- routes/stories.js
+++ routes/stories.js
@@ -42,6 +42,25 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+//@desc Show single story
+//@Route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        res.render('stories/show', {
+            story,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
 // @desc  Show edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -59,6 +78,64 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
         res.render('stories/edit', {
             story,
         })
+    }
+})
+
+//@desc Update Story
+//@Route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true,
+            })
+
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+//@desc Delete story
+//@Route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public',
+        })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
     }
 })
 
